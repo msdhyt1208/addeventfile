ddbfea6451ba498faea8c9c39ed6fc5afea08a29
msdhyt1208 <msdhyt1208@gmail.com>
2023-02-21 16:52:09 +0900

    捲り完了

--- flaxtest/main.js
+++ flaxtest/main.js
@@ -8,12 +8,15 @@ const peel = {
     return function(event){
       // console.log(`X:${this.clientWidth-event.offsetX} Y:${event.offsetY}`),
       strY = event.offsetY; 
-      strX = this.clientWidth-event.offsetX;
-      rad  = Math.acos((strY*strY-strX*strX)/(strX*strX+strY*strY))/Math.PI*180;
-      
-      document.documentElement.style.setProperty('--top', `${strY}px`);
-      document.documentElement.style.setProperty('--left', `${strX}px`);
-      document.documentElement.style.setProperty('--rad', `${rad}deg`);
+      strX = this.clientWidth - event.offsetX;
+      rad  = Math.acos((strY*strY-strX*strX)/(strX*strX+strY*strY));
+      X = strX/Math.sin(rad);
+      Y = strY/Math.sin(rad);
+      // if(Y >= this.clientWidth|| X>= this.clientHeight)return;
+      kakudo = 180-rad/Math.PI*180;
+      document.documentElement.style.setProperty('--top', `${X}px`);
+      document.documentElement.style.setProperty('--left', `${Y}px`);
+      document.documentElement.style.setProperty('--rad', `${kakudo}deg`);
     }
   }
 }
