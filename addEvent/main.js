47567b2e376fa59b317f85e1450251e372b8f611
msdhyt1208 <msdhyt1208@gmail.com>
2023-02-23 19:00:56 +0900

    tarncard実装中

--- addEvent/main.js
+++ addEvent/main.js
@@ -223,7 +223,7 @@ const peel = {
 
   },
   dragover:function(){ 
-    return function(){
+    return function(event){
        console.log("over"); 
        event.preventDefault();
     }
@@ -261,15 +261,122 @@ const peel = {
       document.documentElement.style.setProperty('--left', `${Y}px`);
       document.documentElement.style.setProperty('--rad', `${kakudo}deg`);
     }
-  },
+  }
 
-}
+  }
 peel.action();
 
+//       else{
+//         sqareSt.top = `${500-width}px`;
+//         sqareSt.right = `${500-width}px`;
+//         width = width-X; 
+//       }
+  
+//     }
+//   }
+//   back.addEventListener("click",    motion.click())
+//   back.addEventListener("drop",     motion.drop());
+//   back.addEventListener("dragover", motion.dragover());
+// }
+// turncard();
 
 // setInterval(function(){wheel(scrollBox,1,-50)},100)
 
-
+// function turncard(){
+//   const back = document.querySelector(".back");
+//   const corner = document.querySelector(".corner");
+//   const ol = document.querySelector("ol");
+//   let target = null;
+//   color ={};
+//   records = [];
+//   function rand(){
+//     return Math.floor(Math.random()*255);
+//   }
+  
+//   motion={
+//     click:function(){
+//       return function(){
+//         const text = document.querySelector(".text");
+//         if(text != null ) text.remove();
+      
+//         color={
+//           nambar:{
+//             red:rand(),
+//             blue:rand(),
+//             green:rand()
+//           }
+//         }
+//         color.text = `rgb(${color.nambar.red}, ${color.nambar.green}, ${color.nambar.blue})`;
+//         color.text2= `rgb(${color.nambar.red+30}, ${color.nambar.green+30}, ${color.nambar.blue+30})`;
+//         records.push({...color});
+      
+//         const crLi = document.createElement("li");
+//           crLi.draggable = true;
+//           crLi.textContent = color.text;
+//           crLi.style.backgroundColor = color.text;
+//           crLi.addEventListener("dragstart",motion.dragstart(records.length));
+//           ol.appendChild(crLi);
+      
+//         timer = setInterval(open(false,(back.scrollWidth/150),0),1);
+//       }
+//     },
+//     drop:function(){
+//       return function(event){
+//         this.style.backgroundColor = records[target].text;
+//         event.preventDefault();
+//       }
+//     },
+//     dragover: function(){
+//       return function(event){
+//         event.preventDefault();
+//       }
+//     },
+//     dragstart:function (length){
+//       return function(){
+//         target = length-1;
+//       }
+//     }
+//   }
+//   function open(half,X,width){
+//     return function(){
+//       const backSt = back.style;
+//       const sqareSt = back.firstElementChild.style;
+//       const cornerSt = corner.style;
+  
+//       cornerSt.borderTop  = `solid ${color.text} ${width}px`;
+//       cornerSt.borderLeft = `solid ${color.text2} ${width}px`;
+//       if(width == 0 && !half){
+//         backSt.backgroundColor = color.text;
+//         sqareSt.display = `block`;
+//         sqareSt.top = `0px`;
+//         sqareSt.right = `0px`;
+//       } 
+//       else if(width >= back.scrollWidth && !half){
+//         half = true;
+//         sqareSt.backgroundColor = color.text2;
+//       }
+//       else if(width <= 0 && half){
+//         sqareSt.display = `none`;
+//         clearInterval(timer);
+//         return;
+//       }
+//       if(!half){
+//         width = width+X; 
+//       }
+  
+//       else{
+//         sqareSt.top = `${500-width}px`;
+//         sqareSt.right = `${500-width}px`;
+//         width = width-X; 
+//       }
+  
+//     }
+//   }
+//   back.addEventListener("click",    motion.click())
+//   back.addEventListener("drop",     motion.drop());
+//   back.addEventListener("dragover", motion.dragover());
+// }
+// turncard();
 
 
 addEventListener('abort',       function(event) { console.log(`about`)});
--- flaxtest/main.js
+++ flaxtest/main.js
@@ -1,98 +1,95 @@
-const sqare = document.querySelector(".sqare");
-const corner = document.querySelector(".corner");
-const ol = document.querySelector("ol");
-let target = null;
-color ={};
-
-records = [];
-function rand(){
-  return Math.floor(Math.random(1)*255);
-}
-function open(half,X,width){
-  return function(){
-    if(!half){
-      width = width+X; 
-      corner.style.borderTop = `solid ${color.text} ${width}px`;
-      corner.style.borderLeft = `solid ${color.text2} ${width}px`;
-      sqare.parentNode.style.backgroundColor = color.text;
-    }
-    if(width >= sqare.scrollWidth){
-      half = true;
-    }
-    if(half){
-      width = width-X; 
-      sqare.style.top = `${500-width}px`;
-      sqare.style.right = `${500-width}px`;
-      sqare.style.backgroundColor = `${color.text2}`;
-      corner.style.borderLeft = `solid  ${color.text2} ${width}px`;
-      corner.style.borderTop = `solid   ${color.text}  ${width}px`;
-    }
-    if(width <= 0 && half){
-      sqare.style.top = `0px`;
-      sqare.style.right = `0px`;
-      sqare.style.backgroundColor = color.text;
-      clearInterval(timer);
+function turncard(){
+  const back = document.querySelector(".back");
+  const corner = document.querySelector(".corner");
+  const ol = document.querySelector("ol");
+  let target = null;
+  color ={};
+  records = [];
+  function rand(){
+    return Math.floor(Math.random()*255);
+  }
+  
+  motion={
+    click:function(){
+      return function(){
+        const text = document.querySelector(".text");
+        if(text != null ) text.remove();
+      
+        color={
+          nambar:{
+            red:rand(),
+            blue:rand(),
+            green:rand()
+          }
+        }
+        color.text = `rgb(${color.nambar.red}, ${color.nambar.green}, ${color.nambar.blue})`;
+        color.text2= `rgb(${color.nambar.red+30}, ${color.nambar.green+30}, ${color.nambar.blue+30})`;
+        records.push({...color});
+      
+        const crLi = document.createElement("li");
+          crLi.draggable = true;
+          crLi.textContent = color.text;
+          crLi.style.backgroundColor = color.text;
+          crLi.addEventListener("dragstart",motion.dragstart(records.length));
+          ol.appendChild(crLi);
+      
+        timer = setInterval(open(false,(back.scrollWidth/150),0),1);
+      }
+    },
+    drop:function(){
+      return function(event){
+        this.style.backgroundColor = records[target].text;
+        event.preventDefault();
+      }
+    },
+    dragover: function(){
+      return function(event){
+        event.preventDefault();
+      }
+    },
+    dragstart:function (length){
+      return function(){
+        target = length-1;
+      }
     }
   }
-}
-function close(half,X,width){
-  return function(){
-    if(!half){
-      width = width+X; 
-      corner.style.borderTop = `solid ${color.text} ${width}px`;
-      corner.style.borderLeft = `solid ${color.text2} ${width}px`;
-      sqare.parentNode.style.backgroundColor = color.text;
-    }
-    if(width <= 0){
-      half = false;
-    }
-    if(half){
-      width = width-X; 
-      sqare.style.top = `${500-width}px`;
-      sqare.style.right = `${500-width}px`;
-      sqare.style.backgroundColor = `${color.text2}`;
-      corner.style.borderLeft = `solid  ${color.text2} ${width}px`;
-      corner.style.borderTop = `solid   ${color.text}  ${width}px`;
-    }
-    if(width >= sqare.scrollWidth && !half){
-      sqare.style.top = `0px`;
-      sqare.style.right = `0px`;
-      sqare.style.backgroundColor = color.text;
-      clearInterval(timer);
+  function open(half,X,width){
+    return function(){
+      const backSt = back.style;
+      const sqareSt = back.firstElementChild.style;
+      const cornerSt = corner.style;
+  
+      cornerSt.borderTop  = `solid ${color.text} ${width}px`;
+      cornerSt.borderLeft = `solid ${color.text2} ${width}px`;
+      if(width == 0 && !half){
+        backSt.backgroundColor = color.text;
+        sqareSt.display = `block`;
+        sqareSt.top = `0px`;
+        sqareSt.right = `0px`;
+      } 
+      else if(width >= back.scrollWidth && !half){
+        half = true;
+        sqareSt.backgroundColor = color.text2;
+      }
+      else if(width <= 0 && half){
+        sqareSt.display = `none`;
+        clearInterval(timer);
+        return;
+      }
+      if(!half){
+        width = width+X; 
+      }
+  
+      else{
+        sqareSt.top = `${500-width}px`;
+        sqareSt.right = `${500-width}px`;
+        width = width-X; 
+      }
+  
     }
   }
+  back.addEventListener("click",    motion.click())
+  back.addEventListener("drop",     motion.drop());
+  back.addEventListener("dragover", motion.dragover());
 }
-function dragstart(length){
-  return function(){
-    target = length;
-  }
-}
-sqare.addEventListener("click",function(){
-  half = false;
-  X = sqare.scrollWidth/150;
-  width = 0;
-  color.nambar ={
-    red:rand(),
-    blue:rand(),
-    green:rand()
-  }
-  color.text = `rgb(${color.nambar.red}, ${color.nambar.green}, ${color.nambar.blue})`;
-  color.text2= `rgb(${color.nambar.red+30}, ${color.nambar.green+30}, ${color.nambar.blue+30})`;
-
-  const crLi = document.createElement("li");
-  crLi.draggable = true;
-  crLi.style.backgroundColor = color.text;
-  crLi.addEventListener("dragstart",dragstart(records.length));
-  ol.appendChild(crLi);
-  records.push({...color});
-  console.log(color);
-  timer = setInterval(open(half,X,width),1);
-})
-sqare.addEventListener("drop",function(event){
-  this.style.backgroundColor = records[target].text;
-  event.preventDefault();
-})
-sqare.addEventListener("dragover",function(event){
-  event.preventDefault();
-})
-
+turncard();
